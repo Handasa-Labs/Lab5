99ebf34387c4c2f1a49fc1ff4dd6352faed90441
George <kesaev@gmail.com>
2019-03-28 05:56:24 +0200

    init

---
+++ src/main/java/db/Main.java
@@ -0,0 +1,82 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.net.ssl.SSLException;
+
+public class Main {
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://remotemysql.com/HL8sVJOtq3?useSSL=false";
+	static final String USER = "HL8sVJOtq3";
+	static final String PASS = "MPVWKI2S0e";
+
+	public static void main(String[] args) throws SSLException {
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+			
+			String sql = "SELECT * FROM flights";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				int num = rs.getInt("num");
+				String origin = rs.getString("origin");
+				String destination = rs.getString("destination");
+				int distance = rs.getInt("distance");
+				int price = rs.getInt("price");
+
+				System.out.format("Number %5s Origin %15s destinations %18s Distance %5d Price %5d\n", num, origin, destination, distance, price);
+			}
+			
+			sql = "SELECT origin, destination, distance, num FROM flights";
+			rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				String origin = rs.getString("origin");
+				String destination = rs.getString("destination");
+				int distance = rs.getInt("distance");
+
+				System.out.print("From: " + origin);
+				System.out.print(",\tTo: " + destination);
+				System.out.println(",\t\tDistance: " + distance);
+			}
+			
+			
+			sql = "SELECT origin, destination FROM flights WHERE distance > ?";
+			PreparedStatement prep_stmt = conn.prepareStatement(sql);
+			prep_stmt.setInt(1, 200);
+			rs = prep_stmt.executeQuery();
+			while (rs.next()) {
+				String origin = rs.getString("origin");
+				System.out.println("From: " + origin);
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+			System.out.println("SQLException: " + se.getMessage());
+            System.out.println("SQLState: " + se.getSQLState());
+            System.out.println("VendorError: " + se.getErrorCode());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+}
